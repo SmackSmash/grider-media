2d9cc7d3f2d3e35b291733b158496e1187055260
Dan Moffat <danmoffat@gmail.com>
2024-04-02 21:57:31 +0100

    Added test functions to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { addTest } from './store';
+
 const App = () => {
-  return <h1 className='font-bold text-3xl'>App Works</h1>;
+  const test = useSelector(({ test }) => test);
+  7;
+
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(addTest());
+  };
+
+  return (
+    <main className='p-8'>
+      <h1 className='font-bold text-3xl'>App Works</h1>
+      <button className='bg-lime-600 p-2 my-4 cursor-pointer hover:bg-lime-500' onClick={handleClick}>
+        Add to test
+      </button>
+      {test.length ? test.map(entry => <p key={Math.random()}>{entry}</p>) : <p>No entries</p>}
+    </main>
+  );
 };
 
 export default App;
