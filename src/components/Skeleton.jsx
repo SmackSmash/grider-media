89e6466104bbf8d474fd7065f77db215b0603bc8
Dan Moffat <danmoffat@gmail.com>
2024-04-04 02:09:56 +0100

    A little styling

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -6,8 +6,8 @@ const Skeleton = ({ times, className }) => {
   return Array(times)
     .fill(0)
     .map((_, i) => (
-      <div className={`${className} bg-gray-200 mb-2.5 overflow-hidden rounded opacity-25 relative`} key={i}>
-        <div className='inset-0 absolute -translate-x-full bg-gradient-to-r from-gray-200 via-white to-gray-200 animate-shimmer'></div>
+      <div className={`${className} bg-slate-800 mb-2.5 overflow-hidden rounded relative`} key={i}>
+        <div className='inset-0 absolute -translate-x-full bg-gradient-to-r from-slate-800 via-slate-700 to-slate-800 animate-shimmer'></div>
       </div>
     ));
 };
