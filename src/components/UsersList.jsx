cfc3ac9cf130cf67d4525195da89646cdb2ceb96
Dan Moffat <danmoffat@gmail.com>
2024-04-04 01:18:30 +0100

    Added multiple return statements to handle loading and errors

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -4,6 +4,7 @@ import { fetchUsers } from '../store';
 
 const UsersList = () => {
   const users = useSelector(({ users: { data } }) => data);
+  const isLoading = useSelector(({ users: { isLoading } }) => isLoading);
   const error = useSelector(({ users: { error } }) => error);
 
   const dispatch = useDispatch();
@@ -12,9 +13,16 @@ const UsersList = () => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
-  return (
-    <div>{users && !error ? users.map(({ id, name }) => <p key={id}>{name}</p>) : <p>Something went wrong</p>}</div>
-  );
+  // Good old fashioned if statements work best for multiple return values
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  if (error) {
+    return <div>Something went wrong</div>;
+  }
+
+  return <div>{users && users.map(({ id, name }) => <p key={id}>{name}</p>)}</div>;
 };
 
 export default UsersList;
