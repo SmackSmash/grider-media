1995e9acf7378e415fa7fe161c916f858f525552
Dan Moffat <danmoffat@gmail.com>
2024-04-04 02:57:49 +0100

    Nothing of note

--- src/store/thunks/addUser.js
+++ src/store/thunks/addUser.js
@@ -1,9 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { faker } from '@faker-js/faker';
+import { pause } from '../../utilities';
 
 const addUser = createAsyncThunk('users/add', async () => {
   const response = await axios.post('http://localhost:3005/users', { name: faker.person.fullName() });
+  await pause(300);
   return response.data;
 });
 
